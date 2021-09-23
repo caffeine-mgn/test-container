624f51e2bfc6edb5fc8477622ede37e0b436df6e
caffeine-mgn <caffeine.mgn@gmail.com>
2021-09-23 23:31:22 +0300

    Init

--- buildSrc/src/main/kotlin/pw/binom/Versions.kt
+++ buildSrc/src/main/kotlin/pw/binom/Versions.kt
@@ -4,5 +4,5 @@ object Versions {
     val DOCKER_API_VERSION = "0.1.1"
     val LIB_VERSION = "0.1.1"
     val BINOM_VERSION = "0.1.31"
-    val KOTLIN_VERSION = "1.5.30"
+    val KOTLIN_VERSION = "1.5.31"
 }
