37d0aacf3429e401c3f9eaad8ff159b09f84afdb
subochev <caffeine.mgn@gmail.com>
2024-04-07 12:05:16 +0300

    update kotlin-version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,24 +7,25 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     }
 }
 
+val kotlinVersion = kotlin.coreLibrariesVersion
 val binomVersion = project.property("binom.version") as String
 val dockerApiVersion = project.property("docker_api.version") as String
 val kotlinxCoroutinesVersion = project.property("kotlinx_coroutines.version") as String
 val kotlinxSerializationVersion = project.property("kotlinx_serialization.version") as String
 
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.23"
     id("com.github.gmazzo.buildconfig") version "3.0.3"
 }
 
 buildConfig {
     packageName(project.group.toString())
     buildConfigField("String", "BINOM_VERSION", "\"$binomVersion\"")
-    buildConfigField("String", "KOTLIN_VERSION", "\"${kotlin.coreLibrariesVersion}\"")
+    buildConfigField("String", "KOTLIN_VERSION", "\"${kotlinVersion}\"")
     buildConfigField("String", "DOCKER_API_VERSION", "\"$dockerApiVersion\"")
     buildConfigField("String", "KOTLINX_COROUTINES_VERSION", "\"$kotlinxCoroutinesVersion\"")
     buildConfigField("String", "KOTLINX_SERIALIZATION_VERSION", "\"$kotlinxSerializationVersion\"")
@@ -38,10 +39,10 @@ repositories {
 }
 
 dependencies {
-    api("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
-    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.9.21")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+    api("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:$kotlinVersion")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     api("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
-    api("pw.binom:binom-publish:0.1.14")
+    api("pw.binom:binom-publish:0.1.19")
 }
